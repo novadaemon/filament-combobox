1509d7b300248bfaa5906761041aef51d623c874
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-11-06 10:37:11 -0300

    feat: add new pt_BR language

---
+++ resources/lang/pt_BR/translations.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'input-search' => [
+        'placeholder' => 'Pesquisar...',
+    ],
+    'labels' => [
+        'options' => 'Opções',
+        'selected' => 'Selecionado',
+    ],
+];
