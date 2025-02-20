5112a7a3f82281db2930006a08c12ea39ec7fba5
Yasin Köse <58294158+YasinKose@users.noreply.github.com>
2025-02-20 22:25:42 +0300

    feat(combobox): improve search and bulk operations.
    - Add search state tracking
    - Fix duplicate items handling
    - Enhance bulk move operations with search context
    - Optimize state management

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,9 @@
 export default function cbFormComponent({
-    options,
-    selected,
-    statePath,
-    wire
-}) {
+                                            options,
+                                            selected,
+                                            statePath,
+                                            wire
+                                        }) {
     return {
         optionsOriginal: [],
         options: [],
@@ -12,6 +12,8 @@ export default function cbFormComponent({
         selected: [],
         wire: wire,
         statePath: statePath,
+        isSearchActive: { options: false, selected: false },
+        searchKeyword: { options: '', selected: '' },
         init: function () {
             this.allOptions = options;
             this.prepareRows();
@@ -36,17 +38,17 @@ export default function cbFormComponent({
             })
         },
         search: function (target, keyword) {
+            this.isSearchActive[target] = keyword.length > 0;
+            this.searchKeyword[target] = keyword;
+
             if (target === 'options') {
-                // Only filter items on the left side
                 this.options = this.optionsOriginal.filter(item =>
                     item.label.toLowerCase().includes(keyword.toLowerCase()) &&
-                    !this.selected.some(selectedItem => selectedItem.value === item.value)
+                    !this.selectedOriginal.some(selectedItem => selectedItem.value === item.value)
                 );
             } else if (target === 'selected') {
-                // Only filter items on the right side
-                this.selected = this.selected.filter(item =>
-                    item.label.toLowerCase().includes(keyword.toLowerCase()) &&
-                    !this.options.some(optionItem => optionItem.value === item.value)
+                this.selected = this.selectedOriginal.filter(item =>
+                    item.label.toLowerCase().includes(keyword.toLowerCase())
                 );
             }
         },
@@ -56,12 +58,17 @@ export default function cbFormComponent({
         moveItem: function (item, origin, destiny) {
             this[destiny].push(item)
             this[origin].splice(this[origin].indexOf(item), 1)
+            if (destiny === 'selected') {
+                this.selectedOriginal.push(item);
+            } else if (destiny === 'options') {
+                this.selectedOriginal = this.selectedOriginal.filter(i => i.value !== item.value);
+            }
             this.updateState();
         },
         moveToRight: function () {
             const items = this.options.filter(item => item.highlighted)
             items.map(item => item.highlighted = false)
-            this.selected.push(...items)
+            this.selectedOriginal.push(...items)
             this.options = this.options.filter(item => items.map(i => i.value).indexOf(item.value) === -1)
             this.updateState();
         },
@@ -69,20 +76,44 @@ export default function cbFormComponent({
             const items = this.selected.filter(item => item.highlighted)
             items.map(item => item.highlighted = false)
             this.options.push(...items)
-            this.selected = this.selected.filter(item => items.map(i => i.value).indexOf(item.value) === -1)
+            this.selectedOriginal = this.selectedOriginal.filter(item => !items.some(i => i.value === item.value))
             this.updateState();
         },
         moveToRightAll: function () {
-            this.selected.push(...this.options)
-            this.options = []
+            const itemsToMove = this.isSearchActive.options ? this.options : this.optionsOriginal;
+            this.selectedOriginal.push(...itemsToMove);
+            this.options = [];
+
+            if (this.isSearchActive.options) {
+                this.optionsOriginal = this.optionsOriginal.filter(item =>
+                    !itemsToMove.some(i => i.value === item.value)
+                );
+            } else {
+                this.optionsOriginal = [];
+            }
+
             this.updateState();
         },
         moveToLeftAll: function () {
-            this.options.push(...this.selected)
-            this.selected = []
+            const itemsToMove = this.isSearchActive.selected ? this.selected : this.selectedOriginal;
+            this.options.push(...itemsToMove);
+            this.optionsOriginal.push(...itemsToMove);
+            this.selected = [];
+
+            if (this.isSearchActive.selected) {
+                this.selectedOriginal = this.selectedOriginal.filter(item =>
+                    !itemsToMove.some(i => i.value === item.value)
+                );
+            } else {
+                this.selectedOriginal = [];
+            }
+
             this.updateState();
         },
         updateState: function () {
+            this.options = [...new Map(this.options.map(item => [item.value, item])).values()];
+            this.selectedOriginal = [...new Map(this.selectedOriginal.map(item => [item.value, item])).values()];
+
             const sortByOriginalIndex = (a, b) => {
                 const indexA = this.allOptions.findIndex(o => o.value === a.value);
                 const indexB = this.allOptions.findIndex(o => o.value === b.value);
@@ -90,9 +121,17 @@ export default function cbFormComponent({
             };
 
             this.options.sort(sortByOriginalIndex);
-            this.selected.sort(sortByOriginalIndex);
+            this.selectedOriginal.sort(sortByOriginalIndex);
 
-            wire.dispatchFormEvent('filament-combobox::updateState', this.statePath, this.selected);
+            if (this.isSearchActive.selected) {
+                this.selected = this.selectedOriginal.filter(item =>
+                    item.label.toLowerCase().includes(this.searchKeyword.selected.toLowerCase())
+                );
+            } else {
+                this.selected = [...this.selectedOriginal];
+            }
+
+            wire.dispatchFormEvent('filament-combobox::updateState', this.statePath, this.selectedOriginal);
         }
     }
 }
